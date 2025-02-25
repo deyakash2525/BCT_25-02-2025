8f6a019d301b26319c34637676e0165f17c1948b
deyakash2525 <deyakash2525@gmail.com>
2025-02-25 22:53:23 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,3 +1,14 @@
+# #Task:  User Sign-up/login info
+# 1.create empty array
+# 2.create dictionary for each user containing name,email Id,password.
+# 3.user will put data during Sign-up 
+# 4.then he will login ....if matches with previous data ...pass...or fail
+# 5.data for user1,user2,user3....user n...will be saved
+# 6.it will come in tabular form
+# 7.I can delete any user data too
+
+
+
 from prettytable import PrettyTable 
 users = []
 def sign_up():
